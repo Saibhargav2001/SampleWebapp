091e4911a7b5ec39ec679caa88b1010d6463018a
Saibhargav2001 <63137332+Saibhargav2001@users.noreply.github.com>
2024-04-16 04:07:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,4 @@ import Card from "./UI/Card";
 import IpAddress from "./Components/IpAddress";
 
 function App() {
-  return (
-    <Card>
-      <h1>Your IP address is...</h1>
-      <IpAddress />
-    </Card>
-  );
-}
-
-export default App;
+  ret
